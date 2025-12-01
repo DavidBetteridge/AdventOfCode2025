767bc45bc302af42aa5ede4b1d5239f744c0f0b5
davidbetteridge <david.betteridge@zeromission.io>
2025-12-01 08:35:58 +0000

    Without common

--- Day01/part2.cpp
+++ Day01/part2.cpp
@@ -2,11 +2,17 @@
 
 int main()
 {
-    auto rotations = read_lines_from_file("input.txt");
+    std::ifstream file("input.txt");
+    if (!file.is_open())
+    { 
+        std::cerr << "Error: Could not open file. " << "input.txt" << std::endl;
+        return -1;
+    }
 
+    std::string rotation;
     auto total = 0;
     auto dial = 50;
-    for (const auto &rotation : rotations)
+    while (std::getline(file, rotation))
     {
         auto direction = rotation.substr(0,1);
         auto distance =  std::stoi(rotation.substr(1, rotation.size()-1));
@@ -33,5 +39,5 @@ int main()
     }
 
     std::cout << total << std::endl;
-    return 0;
+    return total;
 }
