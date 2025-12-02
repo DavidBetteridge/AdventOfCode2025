089015648c7b104f40ed91cd5572cc294bdd9a43
davidbetteridge <david.betteridge@zeromission.io>
2025-12-02 11:06:05 +0000

    better

--- Day02/part1.cpp
+++ Day02/part1.cpp
@@ -1,5 +1,7 @@
 #include "common.cpp"
 
+static std::array<int, 8> powers = { 1,10,100,1000,10000,100000,1000000, 10000000 };
+
 int main()
 {
     auto lines = read_lines_from_file("input.txt");
@@ -9,33 +11,20 @@ int main()
     for(const auto &range : ranges)
     {
         auto bounds = split_to_long(range,"-");
-        auto bounds2 = split(range,"-");
-
         for(auto i=bounds[0];i<=bounds[1];i++)
         {
-            auto num = std::to_string(i);
-            if (num.size() % 2 == 0)
-            {
-                auto lhs = std::stoi(num.substr(0, num.size()/2));
-                auto rhs = std::stoi(num.substr(num.size()/2, num.size()));
-                if (lhs == rhs && num.size() % 2 == 0)
-                {
-                //  std::cout << i << std::endl;
-                    total+=i;
-                }
-            }
-        }
+            auto num_length = (int)(log10(i)) + 1;
 
-        // std::cout << bounds[0] << '-' << bounds[1] << ' ' << bounds2[1].size() - bounds2[0].size() << std::endl;
-        // if (bounds2[1].size() == bounds2[0].size())
-        // {
-        //     auto lhs0 = std::stoi(bounds2[0].substr(0, bounds2[0].size()/2));
-        //     auto rhs0 = std::stoi(bounds2[0].substr(bounds2[0].size()/2, bounds2[0].size()));
-        //     auto lhs1 = std::stoi(bounds2[1].substr(0, bounds2[1].size()/2));
-        //     auto rhs1 = std::stoi(bounds2[1].substr(bounds2[1].size()/2, bounds2[1].size()));
-        // }
+            auto a = powers[num_length / 2];
+            auto repeat = i % a;
+            auto times = a + 1;
+
+            if (repeat * times == i )
+                total+=i;
+        }
 
     }
 
+    //28146997880
     std::cout << total << std::endl;
 }
